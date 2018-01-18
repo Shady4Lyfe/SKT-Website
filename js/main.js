9fad0bee96110b3876901e2308ad0d1e32ee6a15
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2018-01-17 21:46:45 -0700

    waretsrydtufygu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,50 @@ let teamNum = 1;
 $("#next").click(nextImg);
 $("#prev").click(prevImg);
 
+$("#Huni").click(function () {
+  window.location.href = "Huni.html"
+});
+
+$("#Untara").click(function () {
+  window.location.href = "Untara.html"
+});
+
+$("#Peanut").click(function () {
+  window.location.href = "Peanut.html"
+});
+
+$("#Blank").click(function () {
+  window.location.href = "Blank.html"
+});
+
+$("#Faker").click(function () {
+  window.location.href = "Faker.html"
+});
+
+$("#Sky").click(function () {
+  window.location.href = "Sky.html"
+});
+
+$("#Bang").click(function () {
+  window.location.href = "Bang.html"
+});
+
+$("#Wolf").click(function () {
+  window.location.href = "Wolf.html"
+});
+
+$("#cCarter").click(function () {
+  window.location.href = "cCarter.html"
+});
+
+$("#kkOma").click(function () {
+  window.location.href = "kkOma.html"
+});
+
+$("#Rapidstar").click(function () {
+  window.location.href = "Rapidstar.html"
+});
+
 function nextImg() {
   $("#team" + teamNum).hide();
 
